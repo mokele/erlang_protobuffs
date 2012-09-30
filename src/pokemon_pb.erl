f7a6efe5b8c632a8be9f4fd808c7dab8efd7f4f0
Steven Gravell <steve@mokele.co.uk>
2012-09-30 01:51:22 +0100

    strings as binaries

--- src/pokemon_pb.erl
+++ src/pokemon_pb.erl
@@ -271,8 +271,6 @@ decode(Bytes, Types, Acc) ->
             end
     end.
     
-unpack_value(Binary, string) when is_binary(Binary) ->
-    binary_to_list(Binary);
 unpack_value(Value, _) -> Value.
     
 to_record(pikachu, DecodedTuples) ->
